0e8ca15b777d954c9277fbfdfc734d2277bea186
ulricusr <ulrich.rueth@gmail.com>
2022-09-25 20:27:00 +0200

    Updated site

--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,49 @@
 ### https://docs.github.com/en/authentication
 ################################################################################
 
+# Variables
+sources_branch="master"
+site_branch="site"
+default_commit_message="Updated site"
+
 # Sanity checks
-all_is_fine = true
+all_is_fine=true
 
 # Check 1: You have to run this script from the directory it resides in
-if ! [[ -f ./deploy.sh ]]; then
-	echo "Error: You need to run this script from the directory deploy.sh resides in."
-	all_is_fine = false
+if ! [[ -f ./deploy.sh ]]
+then
+	echo "Check 2 Error: You need to run this script from the directory deploy.sh resides in"
+	all_is_fine=false
+else
+	echo "Check 1 OK - running in the right directory"
 fi
 
 # Check 2: You have to run this script from the directory it resides in
-if ! [[ ./git status | grep -q 'On branch master' ]]; then
-	echo "Error: You need to be in the master branch of your reäository."
-	all_is_fine = false
+if ! git status | grep -q "On branch $sources_branch"
+then
+	echo "Check 2 Error: You need to be in the $sources_branch branch of your repository"
+	all_is_fine=false
+else
+	echo "Check 2 OK - running in $sources_branch branch"
 fi
 
 # Evaluate checks
-if ! [[ $all_is_fine ]]; then
-	exit 0
-fi	
+if ! $all_is_fine
+then
+	echo "Exiting - nothing checked in, nothing deployed"
+	exit 1
+fi
+
+# Get commit message from the user
+read -p "Enter a commit message (defaults to '$default_commit_message'): " commit_message
+if [[ $commit_message='' ]]
+then
+	echo "Using default commit message: '$default_commit_message'"
+	commit_message=$default_commit_message
+fi
+
+# Add, commit and push
+git add -A
+git commit -m "$commit_message"
+git push
+echo "Pushed sources to remote $sources_branch branch"
